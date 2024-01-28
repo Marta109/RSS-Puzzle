c5a41660993fde7caf9393e19b9c0784ebef26bf
Marta109 <marta.hayrapetyan.a@gmail.com>
2024-01-29 01:12:51 +0400

    feat: add prompt

--- nonograms/script/index.js
+++ nonograms/script/index.js
@@ -15,12 +15,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // ----------- nonograms line and columns
   for (let i = 0; i < 5; i++) {
+    const divPrompt = document.createElement("div");
+    if (i === 0) {
+      divPrompt.classList.add("nonogram-prompt");
+      divPrompt.setAttribute("nonogram-prompt", `${i + 1}`);
+      container.appendChild(divPrompt);
+    }
+
     const divColumns = document.createElement("div");
     divColumns.classList.add("nonogram-columns");
     divColumns.setAttribute("nonogram-columns", "");
+
     // divColumns.textContent = i;
     container.appendChild(divColumns);
+
+    const divPromptItem = document.createElement("div");
+    divPromptItem.classList.add("nonogram-prompt-item_span");
+    divPrompt.appendChild(divPromptItem);
     for (let j = 0; j < 5; j++) {
+      const divPromptItem = document.createElement("div");
+      divPromptItem.classList.add("nonogram-prompt-item");
+      divPromptItem.textContent = `${j + 1}`;
+      for (let k = 0; k < 3; k++) {
+        const span = document.createElement("span");
+        span.textContent = k;
+        divPromptItem.appendChild(span);
+      }
+      // div.textContent = j;
+      divPrompt.appendChild(divPromptItem);
+      // }
+
+      //create div
+      if (j === 0) {
+        const divPromptItem = document.createElement("div");
+        divPromptItem.classList.add("nonogram-prompt-item-column");
+        divColumns.appendChild(divPromptItem);
+        // divPromptItem.textContent = `${j + 1}  2 3 4 5`;
+        for (let k = 0; k < 5; k++) {
+          const p = document.createElement("p");
+          p.textContent = k;
+          divPromptItem.appendChild(p);
+        }
+      }
       const div = document.createElement("div");
       div.classList.add("nonogram-item");
       div.setAttribute("nonogram-line", "");
