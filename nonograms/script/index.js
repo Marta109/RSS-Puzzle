92450bda13b668fbb1dbc4b3bc80382abd9c083a
Marta109 <marta.hayrapetyan.a@gmail.com>
2024-01-29 03:40:58 +0400

    feat: dynamicall prompt add

--- nonograms/script/index.js
+++ nonograms/script/index.js
@@ -30,17 +30,17 @@ document.addEventListener("DOMContentLoaded", () => {
     container.appendChild(divColumns);
 
     const divPromptItem = document.createElement("div");
-    divPromptItem.classList.add("nonogram-prompt-item_span");
+    divPromptItem.classList.add("nonogram-prompt-item_empty");
     divPrompt.appendChild(divPromptItem);
     for (let j = 0; j < 5; j++) {
       const divPromptItem = document.createElement("div");
       divPromptItem.classList.add("nonogram-prompt-item");
-      divPromptItem.textContent = `${j + 1}`;
-      for (let k = 0; k < 3; k++) {
-        const span = document.createElement("span");
-        span.textContent = k;
-        divPromptItem.appendChild(span);
-      }
+      // divPromptItem.textContent = `${j + 1}`; 
+      // for (let k = 0; k < 3; k++) {
+      //   const span = document.createElement("span");
+      //   span.textContent = k;
+      //   divPromptItem.appendChild(span);
+      // }
       // div.textContent = j;
       divPrompt.appendChild(divPromptItem);
       // }
@@ -51,11 +51,11 @@ document.addEventListener("DOMContentLoaded", () => {
         divPromptItem.classList.add("nonogram-prompt-item-column");
         divColumns.appendChild(divPromptItem);
         // divPromptItem.textContent = `${j + 1}  2 3 4 5`;
-        for (let k = 0; k < 5; k++) {
-          const p = document.createElement("p");
-          p.textContent = k;
-          divPromptItem.appendChild(p);
-        }
+        // for (let k = 0; k < 5; k++) {
+        //   const p = document.createElement("p");
+        //   p.textContent = k;
+        //   divPromptItem.appendChild(p);
+        // }
       }
       const div = document.createElement("div");
       div.classList.add("nonogram-item");
--- nonograms/script/main.js
+++ nonograms/script/main.js
@@ -1,6 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const nonogramItems = document.querySelectorAll(".nonogram-item");
 
+  const promptData = {
+    tower: {
+      row: {
+        0: [2],
+        1: [4],
+        2: [3, 1],
+        3: [4],
+        4: [2],
+      },
+      column: {
+        0: [1, 1, 1],
+        1: [5],
+        2: [3],
+        3: [1, 1],
+        4: [3],
+      },
+    },
+  };
+
+  //  ------------- create nonogram prompt Row
+  const createNonogramPromptRow = () => {
+    const nonogramPromptItems = document.querySelectorAll(
+      ".nonogram-prompt-item"
+    );
+
+    nonogramPromptItems.forEach((el, i) => {
+      for (const data of promptData.tower.row[i]) {
+        console.log(data);
+        const span = document.createElement("span");
+        span.classList.add("prompt-span");
+        span.textContent = data;
+        el.appendChild(span);
+      }
+    });
+  };
+
+  createNonogramPromptRow();
+
+  // ------------- create nonogram prompt column
+  const createNonogramPromptColumn = () => {
+    const nonogramPromptItems = document.querySelectorAll(
+      ".nonogram-prompt-item-column"
+    );
+
+    nonogramPromptItems.forEach((el, i) => {
+      console.log(el);
+      for (const data of promptData.tower.column[i]) {
+        console.log(data);
+        const span = document.createElement("span");
+        span.classList.add("prompt-span-column");
+        span.textContent = data;
+        el.appendChild(span);
+      }
+    });
+  };
+
+  createNonogramPromptColumn();
+
+  // -------------  item select
+
+  const nonogramItems = document.querySelectorAll(".nonogram-item");
 
   nonogramItems.forEach((el) => {
     el.addEventListener("mousedown", (e) => {
@@ -18,7 +78,4 @@ document.addEventListener("DOMContentLoaded", () => {
       event.preventDefault();
     });
   });
-
-
-  
 });
