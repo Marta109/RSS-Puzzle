3e23e50a05661300084a7173f90ed076d97427c6
Marta109 <marta.hayrapetyan.a@gmail.com>
2024-03-19 16:28:35 +0400

    fix: build error

--- rss-puzzle/src/data/getData.ts
+++ rss-puzzle/src/data/getData.ts
@@ -1,13 +1,12 @@
 export function getData(
   url: string = 'https://raw.githubusercontent.com/rolling-scopes-school/rss-puzzle-data/main/data/wordCollectionLevel1.json',
 ): Promise<any> {
-  let returnData: any = {};
   return fetch(url)
     .then((response) => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      return (returnData = response.json());
+      return response.json();
     })
     .catch((error) => {
       console.error('There has been a problem with your fetch operation:', error);
